fe81d0ff6c700af3b9a5ebc43771bbbc109d8947
Tung Dinh <72676607+tsdinh442@users.noreply.github.com>
2024-02-17 00:55:17 -0600

    implement tracking

--- 1-object-tracking/optical-flow/optical_flow.py
+++ 1-object-tracking/optical-flow/optical_flow.py
@@ -29,7 +29,8 @@ def select_points(event, x, y, flags, params):
 
         masked_frame = masking(masks, frame, color, opacity=0.5)
 
-        cv2.circle(masked_frame, (x, y), radius=3, color=(0, 255, 0), thickness=-1)
+        for point in points:
+            cv2.circle(masked_frame, point, radius=3, color=(127, 3, 253), thickness=-1)
         cv2.imshow("frame", masked_frame)
 
 def resize(image, scale_factor):
@@ -56,7 +57,7 @@ def segment(image, input_points, input_labels, logits=None, scores=None):
         masks, scores, logits = predictor.predict(
             point_coords=input_points,
             point_labels=input_labels,
-            multimask_output=False,
+            multimask_output=True,
         )
 
     elif len(input_points) > 1:
@@ -94,7 +95,7 @@ def masking(masks, image, color, opacity=0.35):
 
         return masked_image
 
-def optical_flow(video_path):
+def optical_flow(prev_gray, cur_gray, prev_points):
 
     global masks
     global logits
@@ -106,54 +107,72 @@ def optical_flow(video_path):
                      maxLevel=3,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
+    cur_points, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, cur_gray, prev_points, None, **lk_params)
+
+    return cur_points
+
+def track(video_path):
+
+    global masks
+    global logits
+    global scores
+    global color
+
     cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
     frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     cv2.imshow("frame", frame)
     # select points on the picture frame
     cv2.setMouseCallback("frame", select_points, param=frame)
+    key = cv2.waitKey(0)
 
     prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     prev_points = np.array(points).astype(np.float32) if len(points) > 0 else np.array([], dtype=float)
 
-    while True:
-        # allowing the user to pause the video
-        key = cv2.waitKey(33)
-        if key == ord('q'):
-            break
-        elif key == 13:
-            cv2.waitKey(0)
+    if key == 13:
+        n = 0
+        count = 31
+        while True:
+            # allowing the user to pause the video
+            key = cv2.waitKey(33)
+            if key == ord('q'):
+                break
 
-        # read the next frame
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        cur_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # read the next frame
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+            cur_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+            if len(prev_points) > 0:
+                cur_points = optical_flow(prev_gray, cur_gray, prev_points)
 
-        if len(prev_points) > 0:
-            cur_points, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, cur_gray, prev_points, None, **lk_params)
-            print(cur_points)
-            prev_gray = cur_gray
+                if n % 4 == 0:
+                    if len(cur_points) == 1:
+                        masks, scores, logits = segment(frame, np.array(prev_points), np.array(labels))
+                    elif len(cur_points) > 1:
+                        for idx, point in enumerate(cur_points):
+                            if idx == 0:
+                                masks, scores, logits = segment(frame, np.array([point]), np.array([labels[0]]))
+                        masks, scores, logits = segment(frame, np.array(cur_points), np.array(labels), logits=logits, scores=scores)
+
+                    binary_mask = masks[0].astype(np.uint8) * 255
+                    masked_frame = masking(masks, frame, color, opacity=0.5)
+
+                    for (x, y) in cur_points:
+                        point = (int(x), int(y))
+                        cv2.circle(masked_frame, point, radius=3, color=(127, 3, 253), thickness=-1)
+
+                    cv2.imshow("frame", masked_frame)
+                    cv2.imwrite("../../media /out2/frames/" + str(count) + ".png", masked_frame)
+                    cv2.imwrite("../../media /out2/masks/" + str(count) + ".png", binary_mask)
+                    count += 1
+
+                n += 1
+
             prev_points = cur_points
-
-            if len(cur_points) == 1:
-                masks, scores, logits = segment(frame, np.array(points), np.array(labels))
-            elif len(cur_points) > 1:
-                masks, scores, logits = segment(frame, np.array(points), np.array(labels), logits=logits, scores=scores)
-
-            masked_frame = masking(masks, frame, color, opacity=0.5)
-
-            for (x, y) in cur_points:
-                point = (int(x), int(y))
-                cv2.circle(masked_frame, point, radius=3, color=(0, 255, 0), thickness=-1)
-
-            cv2.imshow("frame", masked_frame)
-        
-        else:
-            prev_points = np.array(points).astype(np.float32)
-            cv2.imshow("frame", frame)
+            prev_gray = cur_gray
 
     # Release the VideoWriter object and close the output file
     cap.release()
@@ -166,17 +185,17 @@ model_type = 'vit_h'
 mask_generator, predictor = sam(check_point, model_type)
 
 # media path
-path = "../../media /videos/1.mp4"
+path = "../../media /videos/2.mp4"
 # global variables
 masks = None
 scores = None
 logits = None
 points = []
 labels = []
-color =  (198, 252, 3)
+color = (198, 252, 3)
 
 # run optical flow
-optical_flow(path)
+track(path)
 
 # destroy and exit
 cv2.destroyAllWindows()
