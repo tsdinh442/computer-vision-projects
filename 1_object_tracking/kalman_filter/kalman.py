feed0c79dad4bf324c84fe0b9f761d1b1306bf06
Tung Dinh <72676607+tsdinh442@users.noreply.github.com>
2024-03-10 22:04:29 -0500

    Update Kalman

--- 1_object_tracking/kalman_filter/kalman.py
+++ 1_object_tracking/kalman_filter/kalman.py
@@ -7,30 +7,6 @@ import numpy as np
 
 from utils import manhattan_distance, out_of_bound
 
-def kalman_filter(coordx, coordy):
-
-    stateDim = 4
-    measurementDim = 2
-    dt = 1
-
-    kf = cv2.KalmanFilter(stateDim, measurementDim)  # state vector [x, y, vx, vy] measurement vector [x, y]
-
-    # initialize the kf with starting points
-    kf.statePost = np.array([coordx, coordy, 0, 0], dtype=np.float32)
-
-    # define the transition matrix such that it follows the linear system: _t = x_t-1 + v_xy * dt
-    kf.transitionMatrix = np.array([[1, 0, dt, 0],
-                                    [0, 1, 0, dt],
-                                    [0, 0, 1, 0],
-                                    [0, 0, 0, 1]], dtype=np.float32)
-
-    kf.measurementMatrix = np.array([[1, 0, 0, 0],
-                                     [0, 1, 0, 0]], np.float32)
-
-    kf.measurementNoiseCov = np.array([[1, 0],
-                                       [0, 1]], dtype=np.float32) * 0.05
-
-    return kf
 
 class Kalman_Filter:
 
