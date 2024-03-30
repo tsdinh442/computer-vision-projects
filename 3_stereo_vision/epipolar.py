f1b86a73ecba7e77d8bbb3be8c0f49a26bb02e8d
Tung Dinh <72676607+tsdinh442@users.noreply.github.com>
2024-03-30 15:12:37 -0500

    minor update

--- 3_stereo_vision/epipolar.py
+++ 3_stereo_vision/epipolar.py
@@ -43,48 +43,55 @@ class Epipolar:
 
         ptsLeft, ptsRight = self._get_keypoints()
 
-        self.F, mask = cv2.findFundamentalMat(ptsLeft, ptsRight)
+        self.F, mask = cv2.findFundamentalMat(ptsLeft, ptsRight, cv2.FM_LMEDS)
 
         self.ptsLeft = ptsLeft[mask.ravel() == 1][::5, :]
         self.ptsRight = ptsRight[mask.ravel() == 1][::5, :]
+
+        self.colors = [tuple(np.random.randint(0, 255, 3).tolist()) for i in range(len(self.ptsRight))]
 
     def _get_epilines(self, pts, whichImage):
 
         lines = cv2.computeCorrespondEpilines(pts.reshape(-1, 1, 2), whichImage, self.F)
         return lines.reshape(-1, 3)
 
-    def _draw_epilines(self, pts, lines, pt_image, line_image):
+    def _draw_epilines(self, pts1, pts2, lines, pt_image, line_image):
 
         h, w = line_image.shape
 
         pt_image = cv2.cvtColor(pt_image, cv2.COLOR_GRAY2BGR)
         line_image = cv2.cvtColor(line_image, cv2.COLOR_GRAY2BGR)
 
-        print(len(lines), len(pts))
-        for l, pt in zip(lines, pts):
-            color = tuple(np.random.randint(0, 255, 3).tolist())
+        for l, pt1, pt2, color in zip(lines, pts1, pts2, self.colors):
             start_x, start_y = map(int, [0, -l[2] / l[1]])
             end_x, end_y = map(int, [w, -(l[2] + l[0] * w) / l[1]])
 
-            line_image = cv2.line(line_image, (start_x, start_y), (end_x, end_y), color, 3)
-            pt_image = cv2.circle(pt_image, pt, radius=15, color=color, thickness=-1)
+            line_image = cv2.line(line_image, (start_x, start_y), (end_x, end_y), color=color, thickness=3)
+            line_image = cv2.circle(line_image, pt2, radius=15, color=color, thickness=-1)
+            pt_image = cv2.circle(pt_image, pt1, radius=15, color=color, thickness=-1)
 
         return pt_image, line_image
 
     def draw_epilines_left(self):
         lines = self._get_epilines(self.ptsRight, 2)
-        image_right, image_left = self._draw_epilines(self.ptsRight, lines, self.image_right, self.image_left )
+        _, image_left = self._draw_epilines(self.ptsRight, self.ptsLeft, lines, self.image_right, self.image_left)
 
-        self.show_images(image_left, image_right)
+        return image_left
+        #self.show_images(image_left, image_right)
 
     def draw_epilines_right(self):
         lines = self._get_epilines(self.ptsLeft, 1)
-        image_left, image_right = self._draw_epilines(self.ptsLeft, lines, self.image_left, self.image_right)
+        _, image_right = self._draw_epilines(self.ptsLeft, self.ptsRight, lines, self.image_left, self.image_right)
+        return image_right
+        #self.show_images(image_left, image_right)
 
-        self.show_images(image_left, image_right)
 
+    def show_epilines(self):
 
-    def show_images(self, image_left, image_right):
+        #color = tuple(np.random.randint(0, 255, 3).tolist())
+
+        image_left = self.draw_epilines_left()
+        image_right = self.draw_epilines_right()
 
         plt.figure()
         plt.subplot(121)
@@ -103,5 +110,5 @@ if __name__ == '__main__':
     img_right = '../media/collection/stereo/stereo_right.jpg'
 
     epipolar = Epipolar(img_left, img_right)
-    epipolar.draw_epilines_right()
+    epipolar.show_epilines()
 
